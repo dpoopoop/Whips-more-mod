2e10405a04ef0d4ae7162abe7a0137216bd72956
dpoopoop <81801652+dpoopoop@users.noreply.github.com>
2021-04-04 17:24:13 +0800

    Add files via upload
    temporary

---
+++ ExpandedWeapons/Projectiles/Minions/IceCrystal.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using ExpandedWeapons.Projectiles;
+ 
+namespace ExpandedWeapons.Projectiles.Minions      
+{
+    public class IceCrystal : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.width = 32; 
+            projectile.height = 32;   
+            projectile.hostile = false;  
+            projectile.friendly = false; //false so the crystal itself won't damage enemies on contact.
+            projectile.ignoreWater = true; //does not get slowed in water.    
+            projectile.penetrate = -1; 
+            projectile.tileCollide = false; //does not collide with tiles.
+            projectile.minion = true; //deals summon damage.
+            projectile.alpha = 90; //slightly transparent.
+            projectile.minionSlots = 1f;
+        }
+ 
+        public override void Kill(int timeLeft) //on projectile death
+        {
+ 
+            Main.PlaySound(2, projectile.Center, 8); //play a sound 
+ 
+            for (int i = 0; i < 20; i++) 
+            {
+                //spawn dusts on death
+                int dust = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 160, projectile.velocity.X * 1.2f, projectile.velocity.Y * 1.2f, 160, default(Color), 3.50f);   
+                Main.dust[dust].noGravity = false; 
+                Main.dust[dust].velocity *= 2.5f;
+            }
+        }
+ 
+        public override void AI()
+        {       
+            Player player = Main.player[projectile.owner];
+            if (player.dead || !player.active) {
+				player.ClearBuff(ModContent.BuffType<Buffs.IceCrystal>()); //handling with the summon buff.
+			}
+			if (player.HasBuff(ModContent.BuffType<Buffs.IceCrystal>())) {
+				projectile.timeLeft = 2;
+			}
+            projectile.Center = player.Center - new Vector2(0, 60); //projectile is always above your head.
+            
+            for (int i = 0; i < 200; i++) //this code below handles the aiming and shooting of the minion.
+            {
+                NPC target = Main.npc[i];
+ 
+                
+                float shootToX = target.position.X + (float)target.width * 0.5f - projectile.Center.X; 
+                float shootToY = target.position.Y + (float)target.height * 0.5f - projectile.Center.Y;
+                float distance = (float)System.Math.Sqrt((double)(shootToX * shootToX + shootToY * shootToY));
+ 
+                
+                if (distance < 400f && !target.friendly && target.active && target.CanBeChasedBy()) //range is 400 pixels. 
+                {
+                    if (projectile.ai[0] > 60f) //Fires every second. (60 ticks)
+                    {
+                        
+                        distance = 1.6f / distance;
+ 
+                        
+                        shootToX *= distance * 3;
+                        shootToY *= distance * 3;
+                        int damage = projectile.damage;                  
+                        
+                        Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, shootToX, shootToY, mod.ProjectileType("IceBall"), damage, 4f, Main.myPlayer, 0f, 0f); 
+                        Main.PlaySound(2, (int)projectile.position.X, (int)projectile.position.Y, 28); //28 is the sound
+                        projectile.ai[0] = 0f;
+                    }
+                }
+            }
+            projectile.ai[0] += 1f;
+ 
+        }
+    }
+}
